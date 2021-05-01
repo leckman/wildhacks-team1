fe4ad4c748be0d416092785c08d945f789141b46
emilynkim <83510800+emilynkim@users.noreply.github.com>
2021-05-01 14:38:19 -0700

    Create question2.js

---
+++ question2.js
@@ -0,0 +1,64 @@
+
+function displayMediterraneanResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayItalianResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayAmericanResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayKoreanResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayJapaneseResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayVietnameseResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayThaiResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayChineseResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayMexicanResults() {
+    var result = 'Explore one of these restaurants near you!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
