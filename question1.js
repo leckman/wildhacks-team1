2541982fd2d054f8a6439973734714ba9ad94085
emilynkim <83510800+emilynkim@users.noreply.github.com>
2021-05-01 13:55:59 -0700

    Create question1.js

---
+++ question1.js
@@ -0,0 +1,35 @@
+function displayHappyResults() {
+    var result = 'You should go to the park!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displaySadResults() {
+    var result = 'You should go to the park!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayNeutralResults() {
+    var result = 'You should go to the park!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displaySleepyResults() {
+    var result = 'You should go to the park!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
+  function displayHyperResults() {
+    var result = 'You should go to the park!';
+    
+    resultNode = document.createElement('h4');
+    resultNode.textContent = 'Result: ' + result;
+    document.querySelector('body').appendChild(resultNode);
+  }
