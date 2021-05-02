07f653861b5c9637020037eb51d50db13c53ddf1
emilynkim <83510800+emilynkim@users.noreply.github.com>
2021-05-01 17:11:44 -0700

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,33 +1,33 @@
 function displayHappyResults() {
-    var result = 'You should go to the park!';
+    var result = 'You should go shopping!';
     
     resultNode = document.createElement('h4');
     resultNode.textContent = 'Result: ' + result;
     document.querySelector('body').appendChild(resultNode);
   }
   function displaySadResults() {
-    var result = 'You should go to the park!';
+    var result = 'You should go meet up with friends!';
     
     resultNode = document.createElement('h4');
     resultNode.textContent = 'Result: ' + result;
     document.querySelector('body').appendChild(resultNode);
   }
   function displayNeutralResults() {
-    var result = 'You should go to the park!';
+    var result = 'You should go get some food!';
     
     resultNode = document.createElement('h4');
     resultNode.textContent = 'Result: ' + result;
     document.querySelector('body').appendChild(resultNode);
   }
   function displaySleepyResults() {
-    var result = 'You should go to the park!';
+    var result = 'You should go to take a walk in the park!';
     
     resultNode = document.createElement('h4');
     resultNode.textContent = 'Result: ' + result;
     document.querySelector('body').appendChild(resultNode);
   }
   function displayHyperResults() {
-    var result = 'You should go to the park!';
+    var result = 'You should go to yoga or meditation class!';
     
     resultNode = document.createElement('h4');
     resultNode.textContent = 'Result: ' + result;
